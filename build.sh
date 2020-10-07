37b540385249f576c8059223ac033072a4431582
rfaircloth-splunk <rfaircloth@splunk.com>
2020-10-07 15:11:05 -0400

    test

--- build.sh
+++ build.sh
@@ -11,42 +11,40 @@ then
 fi
 for line in `cat .python-version`; do pyenv install -s "$line"; done
 # echo "Installing Virtual Environment"
+pip3 install virtualenv > /dev/null
+if [ ! -d "./.venv" ]; then
+python3 -m virtualenv .venv -p python3 > /dev/null
+fi
+source .venv/bin/activate
+echo "Virtual Environment Installed and Activated"
 # echo "Installing Dependencies"
-echo install setuptools
-pip3 install setuptools --upgrade
-pip3 install wheel --upgrade
-pip3 install --upgrade pip
-pip2 install --upgrade pip
-echo install slim
-pip3 install https://download.splunk.com/misc/packaging-toolkit/splunk-packaging-toolkit-1.0.1.tar.gz --upgrade
-echo install requirements_dev
 pip3 install -r requirements_dev.txt --upgrade
 
 PACKAGE_ID=$(crudini --get package/default/app.conf id name)
-VERSION=$(./semtag getcurrent)
-BUILD_DIR=build/source/$PACKAGE_ID
+# VERSION=$(./semtag getcurrent)
+# BUILD_DIR=build/source/$PACKAGE_ID
 
-export VERSION_SPLUNK=$(python ./splver.py ${VERSION})
-export PACKAGE_BUILD=$(date +%s)
-rm -rf build || true
-rm -rf output || true
-mkdir -p $BUILD_DIR
+# # export VERSION_SPLUNK=$(python ./splver.py ${VERSION})
+# # export PACKAGE_BUILD=$(date +%s)
+# # rm -rf build || true
+# # rm -rf output || true
+# # mkdir -p $BUILD_DIR
 
-ucc-gen --ta-version $VERSION_SPLUNK
-cp -r output/$PACKAGE_ID/* $BUILD_DIR/
-rm -rf $BUILD_DIR/samples
-rm -rf $BUILD_DIR/default/eventgen.conf
-crudini --set $BUILD_DIR/default/app.conf launcher version $VERSION_SPLUNK
-crudini --set $BUILD_DIR/default/app.conf id version $VERSION_SPLUNK
-crudini --set $BUILD_DIR/default/app.conf install build ${PACKAGE_BUILD}
-cp -r LICENSES $BUILD_DIR/
+# # ucc-gen --ta-version $VERSION_SPLUNK
+# # cp -r output/$PACKAGE_ID/* $BUILD_DIR/
+# # rm -rf $BUILD_DIR/samples
+# # rm -rf $BUILD_DIR/default/eventgen.conf
+# # crudini --set $BUILD_DIR/default/app.conf launcher version $VERSION_SPLUNK
+# # crudini --set $BUILD_DIR/default/app.conf id version $VERSION_SPLUNK
+# # crudini --set $BUILD_DIR/default/app.conf install build ${PACKAGE_BUILD}
+# # cp -r LICENSES $BUILD_DIR/
 
-slim generate-manifest $BUILD_DIR --update >/tmp/app.manifest   || true
-cp  /tmp/app.manifest  $BUILD_DIR/app.manifest
-mkdir -p build/package/splunkbase
-mkdir -p build/package/deployment
-slim package -o build/package/splunkbase $BUILD_DIR 
+# # slim generate-manifest $BUILD_DIR --update >/tmp/app.manifest   || true
+# # cp  /tmp/app.manifest  $BUILD_DIR/app.manifest
+# # mkdir -p build/package/splunkbase
+# # mkdir -p build/package/deployment
+# # slim package -o build/package/splunkbase $BUILD_DIR 
 
-mkdir -p build/package/deployment
-PACKAGE=$(ls build/package/splunkbase/*)
-#slim partition -o build/package/deployment $PACKAGE || true
+# # mkdir -p build/package/deployment
+# # PACKAGE=$(ls build/package/splunkbase/*)
+# # slim partition -o build/package/deployment $PACKAGE || true
