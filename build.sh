ab8f548d202a24b916a3f876a170170ea0f6c375
rfaircloth-splunk <rfaircloth@splunk.com>
2020-10-07 15:43:56 -0400

    Update build.sh

--- build.sh
+++ build.sh
@@ -22,29 +22,29 @@ pip3 install -r requirements_dev.txt --upgrade
 
 PACKAGE_ID=$(crudini --get package/default/app.conf id name)
 
-# BUILD_DIR=build/source/$PACKAGE_ID
+BUILD_DIR=build/source/$PACKAGE_ID
 
-# # export VERSION_SPLUNK=$(python ./splver.py ${VERSION})
-# # export PACKAGE_BUILD=$(date +%s)
-# # rm -rf build || true
-# # rm -rf output || true
-# # mkdir -p $BUILD_DIR
+export VERSION_SPLUNK=$(python ./splver.py ${VERSION})
+export PACKAGE_BUILD=$(date +%s)
+rm -rf build || true
+rm -rf output || true
+mkdir -p $BUILD_DIR
 
-# # ucc-gen --ta-version $VERSION_SPLUNK
-# # cp -r output/$PACKAGE_ID/* $BUILD_DIR/
-# # rm -rf $BUILD_DIR/samples
-# # rm -rf $BUILD_DIR/default/eventgen.conf
-# # crudini --set $BUILD_DIR/default/app.conf launcher version $VERSION_SPLUNK
-# # crudini --set $BUILD_DIR/default/app.conf id version $VERSION_SPLUNK
-# # crudini --set $BUILD_DIR/default/app.conf install build ${PACKAGE_BUILD}
-# # cp -r LICENSES $BUILD_DIR/
+ucc-gen --ta-version $VERSION_SPLUNK
+cp -r output/$PACKAGE_ID/* $BUILD_DIR/
+rm -rf $BUILD_DIR/samples
+rm -rf $BUILD_DIR/default/eventgen.conf
+crudini --set $BUILD_DIR/default/app.conf launcher version $VERSION_SPLUNK
+crudini --set $BUILD_DIR/default/app.conf id version $VERSION_SPLUNK
+crudini --set $BUILD_DIR/default/app.conf install build ${PACKAGE_BUILD}
+cp -r LICENSES $BUILD_DIR/
 
-# # slim generate-manifest $BUILD_DIR --update >/tmp/app.manifest   || true
-# # cp  /tmp/app.manifest  $BUILD_DIR/app.manifest
-# # mkdir -p build/package/splunkbase
-# # mkdir -p build/package/deployment
-# # slim package -o build/package/splunkbase $BUILD_DIR 
+slim generate-manifest $BUILD_DIR --update >/tmp/app.manifest   || true
+cp  /tmp/app.manifest  $BUILD_DIR/app.manifest
+mkdir -p build/package/splunkbase
+mkdir -p build/package/deployment
+slim package -o build/package/splunkbase $BUILD_DIR 
 
-# # mkdir -p build/package/deployment
-# # PACKAGE=$(ls build/package/splunkbase/*)
-# # slim partition -o build/package/deployment $PACKAGE || true
+mkdir -p build/package/deployment
+PACKAGE=$(ls build/package/splunkbase/*)
+slim partition -o build/package/deployment $PACKAGE || true
