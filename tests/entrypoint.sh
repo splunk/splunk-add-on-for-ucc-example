b72bfbb5e83a80378b0e7af3694979c4f6704244
monshah <monshah@splunk.com>
2020-10-15 12:22:26 +0530

    Added-if-else

--- tests/entrypoint.sh
+++ tests/entrypoint.sh
@@ -6,5 +6,12 @@
 ##
 
 cd /home/circleci/work
+if [${TEST_SET} -eq "tests/ui"]; then
+    echo "Hello1"
+    cp /home/circleci/work/tests/pytest-ko.ini /home/circleci/work/tests/pytest.ini
+    echo "Hello"
+else
+    cp /home/circleci/work/tests/pytest-ci.ini /home/circleci/work/tests/pytest.ini
+fi
 echo Test Args $@ ${TEST_SET}
 pytest $@ ${TEST_SET}
