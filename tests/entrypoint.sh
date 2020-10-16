b4c13a455065d35e67d4395637fb21056785643c
Anil Vaza (C) <avaza@splunk.com>
2020-10-16 16:16:44 +0530

    Updated entrypoint file

--- tests/entrypoint.sh
+++ tests/entrypoint.sh
@@ -9,11 +9,8 @@ curl https://saucelabs.com/downloads/sc-4.4.12-linux.tar.gz -o /home/circleci/wo
 tar -xzf /home/circleci/work/saucelabs.tar.gz --directory /home/circleci/work/
 cd /home/circleci/work/sc-*
 /home/circleci/work/sc-4.4.12-linux/bin/sc -u ta-factory-01 -k be7beeab-8bba-420c-90b3-8eac2d16d3ca &
-# wget --retry-connrefused --no-check-certificate -T 10 localhost:4445
+wget --retry-connrefused --no-check-certificate -T 10 localhost:4445
 
-while ! nc -z localhost 4445; do
-    sleep 1
-done
 
 cd /home/circleci/work
 ls -la
