f7be242a524be7024ea1745304dc37e052fa02a3
Anil Vaza (C) <avaza@splunk.com>
2020-10-16 10:59:33 +0530

    Updated the entrypoint script

--- tests/entrypoint.sh
+++ tests/entrypoint.sh
@@ -5,11 +5,12 @@
 ##
 ##
 
-curl https://saucelabs.com/downloads/sc-4.4.12-linux.tar.gz -o /home/circleci/work/tests/saucelabs.tar.gz
-tar -xzf /home/circleci/work/tests/saucelabs.tar.gz --directory /home/circleci/work/tests/
-cd /home/circleci/work/tests/sc-*
-/home/circleci/work/tests/sc-4.4.12-linux/bin/sc -u ta-factory-01 -k be7beeab-8bba-420c-90b3-8eac2d16d3ca &
+curl https://saucelabs.com/downloads/sc-4.4.12-linux.tar.gz -o /home/circleci/work/saucelabs.tar.gz
+tar -xzf /home/circleci/work/saucelabs.tar.gz --directory /home/circleci/work/
+cd /home/circleci/work/sc-*
+/home/circleci/work/sc-4.4.12-linux/bin/sc -u ta-factory-01 -k be7beeab-8bba-420c-90b3-8eac2d16d3ca &
 wget --retry-connrefused --no-check-certificate -T 6 localhost:4445
 
 cd /home/circleci/work
+ls -la
 pytest $@
