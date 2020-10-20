cb441542b75e693516796599ec2cd948d51d0dae
Anil Vaza (C) <avaza@splunk.com>
2020-10-20 14:18:01 +0530

    Updated docker files

--- tests/entrypoint.sh
+++ tests/entrypoint.sh
@@ -11,8 +11,8 @@ cd /home/circleci/work/sc-*
 /home/circleci/work/sc-4.4.12-linux/bin/sc -u ta-factory-01 -k be7beeab-8bba-420c-90b3-8eac2d16d3ca &
 wget --retry-connrefused --no-check-certificate -T 10 localhost:4445
 
-ls -la /home/circleci/work/
-ip=$(head -n 1 /home/circleci/work/ip.txt)
+ls -la /home/circleci/
+ip=$(head -n 1 /home/circleci/ip.txt)
 
 echo "ip arg: $ip"
 cd /home/circleci/work
