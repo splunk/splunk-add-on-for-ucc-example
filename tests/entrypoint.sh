57b59d8785d10a5adfffe1f205f547b226465a62
Anil Vaza (C) <avaza@splunk.com>
2020-10-21 17:53:38 +0530

    Move saucelab code to circleci config file

--- tests/entrypoint.sh
+++ tests/entrypoint.sh
@@ -5,12 +5,6 @@
 ##
 ##
 
-curl https://saucelabs.com/downloads/sc-4.4.12-linux.tar.gz -o /home/circleci/work/saucelabs.tar.gz
-tar -xzf /home/circleci/work/saucelabs.tar.gz --directory /home/circleci/work/
-cd /home/circleci/work/sc-*
-/home/circleci/work/sc-4.4.12-linux/bin/sc -u ta-factory-01 -k be7beeab-8bba-420c-90b3-8eac2d16d3ca &
-wget --retry-connrefused --no-check-certificate -T 10 localhost:4445
-
 echo "ip arg: $1"
 echo "web port arg: $2"
 cd /home/circleci/work
