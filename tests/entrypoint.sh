222448005e26245fc9ebbe1a137f5364064f0c0d
Anil Vaza (C) <avaza@splunk.com>
2020-10-21 11:30:20 +0530

    Updated entrypoint file

--- tests/entrypoint.sh
+++ tests/entrypoint.sh
@@ -13,4 +13,5 @@ wget --retry-connrefused --no-check-certificate -T 10 localhost:4445
 echo "ip arg: $1"
 cd /home/circleci/work
 ls -la 
+cat /home/circleci/work/pytest.ini
 pytest $@ --splunk-host=$1
