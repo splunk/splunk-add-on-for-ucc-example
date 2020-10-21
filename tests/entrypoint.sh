91298e833ab8c8cdc839788d53eccb69184b2157
Anil Vaza (C) <avaza@splunk.com>
2020-10-21 18:49:57 +0530

    Updated entrypint

--- tests/entrypoint.sh
+++ tests/entrypoint.sh
@@ -7,7 +7,8 @@
 
 echo "ip arg: $1"
 echo "web port arg: $2"
+echo "port arg: $3"
 cd /home/circleci/work
 ls -la 
-pytest --splunk-host=$1 --splunkweb-port=$2 --splunk-password=Chang3d!
+pytest --splunk-host=$1 --splunkweb-port=$2 --splunk-port=$3 --splunk-password=Chang3d!
 touch /home/circleci/work/docker_exit
