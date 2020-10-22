9fffa5726df92f5706bed2b02d313542bfa3f7dc
Jay Joshi <37693483+jayjoshi-crest@users.noreply.github.com>
2020-10-22 22:54:13 +0530

    CircleCI SauceLabs Integration-2

--- tests/entrypoint.sh
+++ tests/entrypoint.sh
@@ -4,13 +4,13 @@
 ## SPDX-License-Identifier: LicenseRef-Splunk-1-2020
 ##
 ##
+echo "Starting SauceLabs Connection..."
+curl https://saucelabs.com/downloads/sc-4.4.12-linux.tar.gz -o /home/circleci/saucelabs.tar.gz
+tar -xzf /home/circleci/saucelabs.tar.gz --directory /home/circleci/
+/home/circleci/sc-4.4.12-linux/bin/sc -u ta-factory-01 -k be7beeab-8bba-420c-90b3-8eac2d16d3ca -i myTunnelO1 --no-remove-colliding-tunnels -v &
+wget --retry-connrefused --no-check-certificate -T 10 localhost:4445
 
-echo "ip arg: $1"
-echo "web port arg: $2"
-echo "port arg: $3"
 cd /home/circleci/work
 ls -la 
-pip install pytest-timeout
-pytest --splunk-host=$1 --splunkweb-port=$2 --splunk-port=$3 --splunk-password=Chang3d!  --timeout=300
-tail -n 100 $(find . -name pytest_splunk_addon.log)
+pytest --splunk-host=splunk --splunkweb-port=8000 --splunk-port=8089 --splunk-password=Chang3d!
 touch /home/circleci/work/docker_exit
