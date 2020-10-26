9a69e73324ca2782389c495b60ba8a7287077bf8
jayjoshi-crest <jjoshi@splunk.com>
2020-10-26 21:55:24 +0530

    Added sauce connect in config.yml

--- tests/entrypoint.sh
+++ tests/entrypoint.sh
@@ -4,11 +4,6 @@
 ## SPDX-License-Identifier: LicenseRef-Splunk-1-2020
 ##
 ##
-echo "Starting SauceLabs Connection..."
-curl https://saucelabs.com/downloads/sc-4.4.12-linux.tar.gz -o /home/circleci/saucelabs.tar.gz
-tar -xzf /home/circleci/saucelabs.tar.gz --directory /home/circleci/
-/home/circleci/sc-4.4.12-linux/bin/sc -u ta-factory-01 -k be7beeab-8bba-420c-90b3-8eac2d16d3ca -i myTunnelO1 --no-remove-colliding-tunnels -v &
-wget --retry-connrefused --no-check-certificate -T 10 localhost:4445
 
 cd /home/circleci/work
 ls -la 
