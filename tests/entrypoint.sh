1f19b50ae57c37a321e963e77863f07d8ad92f22
jayjoshi-crest <jjoshi@splunk.com>
2020-10-28 11:35:29 +0530

    CircleCI integration with Saucelabs

--- tests/entrypoint.sh
+++ tests/entrypoint.sh
@@ -9,5 +9,10 @@ cd /home/circleci/work
 if [ -f "${TEST_SET}/pytest-ci.ini" ]; then
     cp -f ${TEST_SET}/pytest-ci.ini ${TEST_SET}/pytest.ini
 fi
+
+echo "Check Saucelab connection..."
+wget --retry-connrefused --no-check-certificate -T 10 sauceconnect:4445
+
+echo "Executing Tests..."
 echo Test Args $@ ${TEST_SET}
 pytest $@ ${TEST_SET}
