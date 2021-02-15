abf26cccde216a47f0a49603c85a30c08498805a
arjunkhunti-crest <akhunti@splunk.com>
2021-02-15 12:15:40 +0530

    test: added semgrep_test file

--- tests/knowledge/test_addon.py
+++ tests/knowledge/test_addon.py
@@ -4,15 +4,9 @@
 #
 #
 import pytest
-import requests
 from pytest_splunk_addon.standard_lib.addon_basic import Basic
 
 
 class Test_App(Basic):
     def empty_method(self):
-        if 1 == 1:
-            pass
-
-    def dummy_method(self):
-        r = requests.post("http://www.github.com", auth=('user', 'pass'))
-
+        pass
